39bdcaa8efb017c2d01b40f6c79c734ed13323f8
Lahlukap669 <46345228+Lahlukap669@users.noreply.github.com>
2020-03-31 15:36:38 +0200

    initial upload

---
+++ maps_hlastnik_git/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace maps_hlastnik
+{
+    partial class Map_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.Label_1 = new System.Windows.Forms.Label();
+            this.StartLocation = new System.Windows.Forms.TextBox();
+            this.Load_Location = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EndLocation = new System.Windows.Forms.TextBox();
+            this.Display = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(236, 465);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(242, 0);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(572, 465);
+            this.gMapControl1.TabIndex = 1;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // Label_1
+            // 
+            this.Label_1.AutoSize = true;
+            this.Label_1.Location = new System.Drawing.Point(13, 9);
+            this.Label_1.Name = "Label_1";
+            this.Label_1.Size = new System.Drawing.Size(86, 13);
+            this.Label_1.TabIndex = 2;
+            this.Label_1.Text = "Začetna lokacija";
+            // 
+            // StartLocation
+            // 
+            this.StartLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.StartLocation.Location = new System.Drawing.Point(16, 25);
+            this.StartLocation.Name = "StartLocation";
+            this.StartLocation.Size = new System.Drawing.Size(206, 24);
+            this.StartLocation.TabIndex = 3;
+            // 
+            // Load_Location
+            // 
+            this.Load_Location.Location = new System.Drawing.Point(16, 112);
+            this.Load_Location.Name = "Load_Location";
+            this.Load_Location.Size = new System.Drawing.Size(206, 36);
+            this.Load_Location.TabIndex = 4;
+            this.Load_Location.Text = "Najdi";
+            this.Load_Location.UseVisualStyleBackColor = true;
+            this.Load_Location.Click += new System.EventHandler(this.Load_Location_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Končna lokacija";
+            // 
+            // EndLocation
+            // 
+            this.EndLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.EndLocation.Location = new System.Drawing.Point(16, 73);
+            this.EndLocation.Name = "EndLocation";
+            this.EndLocation.Size = new System.Drawing.Size(206, 24);
+            this.EndLocation.TabIndex = 6;
+            // 
+            // Display
+            // 
+            this.Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F);
+            this.Display.FormattingEnabled = true;
+            this.Display.Location = new System.Drawing.Point(16, 191);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(206, 199);
+            this.Display.TabIndex = 7;
+            // 
+            // Map_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 465);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.EndLocation);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Load_Location);
+            this.Controls.Add(this.StartLocation);
+            this.Controls.Add(this.Label_1);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Map_form";
+            this.Text = "Zemljevid";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Label Label_1;
+        private System.Windows.Forms.TextBox StartLocation;
+        private System.Windows.Forms.Button Load_Location;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EndLocation;
+        private System.Windows.Forms.ListBox Display;
+    }
+}
+
---
+++ maps_hlastnik_git/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET;
+
+namespace maps_hlastnik
+{
+    public partial class Map_form : Form
+    {
+        
+
+        public Map_form()
+        {
+            InitializeComponent();
+
+            try
+            {
+                System.Net.IPHostEntry e =
+                     System.Net.Dns.GetHostEntry("www.google.com");
+            }
+            catch
+            {
+                gMapControl1.Manager.Mode = AccessMode.CacheOnly;
+                MessageBox.Show("No internet connection avaible, going to CacheOnly mode.",
+                      "GMap.NET - Demo.WindowsForms", MessageBoxButtons.OK,
+                      MessageBoxIcon.Warning);
+            }
+
+            gMapControl1.MapProvider = GMapProviders.OpenStreetMap;
+            gMapControl1.Position = new PointLatLng(46.357, 15.129);
+            gMapControl1.MinZoom = 0;
+            gMapControl1.MaxZoom = 24;
+            gMapControl1.Zoom = 9;
+        }
+
+        public void cMarker(double lat, double lon, string mesto, double tmp, double wind)
+        {
+            PointLatLng point = new PointLatLng(lat, lon);
+            GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.red_small);
+            marker.ToolTipText = mesto + ": " + "Temp: " + tmp.ToString() + "  Wind: " + wind.ToString();
+
+            GMapOverlay markers = new GMapOverlay("markers");
+            markers.Markers.Add(marker);
+            gMapControl1.Overlays.Add(markers);
+        }
+
+        
+
+        private void Load_Location_Click(object sender, EventArgs e)
+        {
+
+
+            HttpClient request = new HttpClient();
+
+            request.BaseAddress = new Uri($"https://best-way.herokuapp.com/api/between/"+ StartLocation.Text + "/"+EndLocation.Text);
+
+            request.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = request.GetAsync("").Result;
+            if (response.IsSuccessStatusCode)
+            { 
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                dynamic json_o = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+                gMapControl1.Overlays.Clear();
+                Display.Items.Clear();
+                try
+                {
+                    for (int x = 0; x < 5; x++)
+                {
+                    
+                        double lon = json_o.result[x].location.longitude;
+                        double lat = json_o.result[x].location.latitude;
+                        string mesto = json_o.result[x].weather.city;
+                        double tmp = json_o.result[x].weather.temp;
+                        double wind = json_o.result[x].weather.wind;
+                        Display.Items.Add("Mesto: " + mesto + ", temp: " + tmp + ", veter: " + wind);
+                        cMarker(lat, lon, mesto, tmp, wind);
+                        Console.WriteLine(lon + " " + lat);
+                    
+                    }
+                }
+                catch (Exception e1)
+                {
+                    MessageBox.Show("Couldn't find points between locations "+ StartLocation.Text + " and " + EndLocation.Text);
+                    Console.WriteLine(e1.Message.ToString());
+                }
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+
+            request.Dispose();
+        }
+    }
+}
